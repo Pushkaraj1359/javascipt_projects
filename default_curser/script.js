2b8af75380206af24672df38b6576bd3f01a04a5
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2024-06-28 23:06:31 +0530

    commit is added

--- default_curser/script.js
+++ default_curser/script.js
@@ -1,6 +1,8 @@
 let main=document.querySelector(".main");
 let box=document.querySelector(".box");
 
+
+// play with event position X-axis and Y-axis
 main.addEventListener("mousemove",function(dets){
     box.style.left= dets.x+"px";
     box.style.top=dets.y+"px";
